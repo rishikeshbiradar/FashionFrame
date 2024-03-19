6dcbb7a63fe0c39e739dd1997e8162b6376e067a
rishikeshbiradar <rushisb.55@gmail.com>
2024-03-19 23:43:00 +0530

    Added Files

---
+++ src/App.js
@@ -0,0 +1,221 @@
+import React, { useState, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import prettier from 'prettier';
+
+const initialProducts = [
+  {
+    id: uuidv4(),
+    image: "https://images.pexels.com/photos/6310924/pexels-photo-6310924.jpeg?auto=compress&cs=tinysrgb&w=600",
+    name: "jeshvi fashion Men Solid Casual Dark Blue, Green Shirt",
+    gender: 'Men',
+    price: 670,
+    color: 'Dark Blue and Green',
+    size: 'XL',
+  },
+  {
+    id: uuidv4(),
+    image: "https://images.pexels.com/photos/9286143/pexels-photo-9286143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name: "MISS CLOTHING Women Kurta Dupatta Set",
+    gender: 'Women',
+    price: 690,
+    color: 'White and Pink',
+    size: 'L',
+  },
+];
+
+const App = () => {
+  const [products, setProducts] = useState(initialProducts);
+  const [newProductImage, setNewProductImage] = useState('');
+  const [newProductName, setNewProductName] = useState('');
+  const [newProductGender, setNewProductGender] = useState('');
+  const [newProductPrice, setNewProductPrice] = useState('');
+  const [newProductColor, setNewProductColor] = useState('');
+  const [newProductSize, setNewProductSize] = useState('');
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const items = Array.from(products);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setProducts(items);
+  };
+
+  const addProduct = () => {
+    setProducts([
+      ...products,
+      {
+        id: uuidv4(),
+        image: newProductImage || 'https://via.placeholder.com/150',
+        name: newProductName,
+        gender: newProductGender,
+        price: parseFloat(newProductPrice),
+        color: newProductColor,
+        size: newProductSize,
+        fabric: 'Cotton', // For simplicity, fabric is set to default here, you can modify it as needed
+      },
+    ]);
+    // Clear input fields after adding the product
+    setNewProductImage('');
+    setNewProductName('');
+    setNewProductGender('');
+    setNewProductPrice('');
+    setNewProductColor('');
+    setNewProductSize('')
+
+  };
+
+  const deleteProduct = (id) => {
+    const filteredProducts = products.filter((product) => product.id !== id);
+    setProducts(filteredProducts);
+  };
+
+  const codeToCopyRef = useRef(null);
+  const [copied, setCopied] = useState(false);
+
+  const formatHTML = (html) => {
+    const tab = '  '; // Two spaces for indentation
+    let formattedHTML = '';
+    let indentLevel = 0;
+
+    html.split(/>\s*</).forEach((element) => {
+      if (element.match(/^\/\w/)) {
+        indentLevel--;
+      }
+
+      formattedHTML += `${tab.repeat(indentLevel)}<${element}>\n`;
+
+      if (element.match(/^<?\w[^>]*[^/]$/)) {
+        indentLevel++;
+      }
+    });
+
+    return formattedHTML.trim();
+  };
+
+  const copyCode = () => {
+    const codeToCopy = formatHTML(codeToCopyRef.current.innerHTML);
+    navigator.clipboard.writeText(codeToCopy)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => {
+          setCopied(false);
+        }, 1500); // Reset copied state after 1.5 seconds
+      })
+      .catch(err => {
+        console.error('Failed to copy: ', err);
+      });
+  };
+  return (
+    <div>
+      <div ref={codeToCopyRef} className="container mx-auto">
+        <div className="mb-4">
+          <input
+            type="text"
+            value={newProductImage}
+            onChange={(e) => setNewProductImage(e.target.value)}
+            placeholder="Enter Image URL"
+            className="border border-gray-400 p-2 rounded m-1"
+          />
+          <input
+            type="text"
+            value={newProductName}
+            onChange={(e) => setNewProductName(e.target.value)}
+            placeholder="Enter Product Name"
+            className="border border-gray-400 p-2 rounded m-1 "
+          />
+          <input
+            type="number"
+            value={newProductPrice}
+            onChange={(e) => setNewProductPrice(e.target.value)}
+            placeholder="Enter Price"
+            className="border border-gray-400 p-2 rounded m-1 mx-2"
+          />
+          <input
+            type="text"
+            value={newProductGender}
+            onChange={(e) => setNewProductGender(e.target.value)}
+            placeholder="Enter Gender"
+            className="border border-gray-400 p-2 rounded m-1"
+          />
+
+          <input
+            type="text"
+            value={newProductColor}
+            onChange={(e) => setNewProductColor(e.target.value)}
+            placeholder="Enter Color"
+            className="border border-gray-400 p-2 rounded m-1 mx-2"
+          />
+          <input
+            type="text"
+            value={newProductSize}
+            onChange={(e) => setNewProductSize(e.target.value)}
+            placeholder="Enter Size"
+            className="border border-gray-400 p-2 rounded m-1"
+          />
+          <div className="flex justify-end">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded"
+              onClick={addProduct}
+            >
+              Add Product
+            </button>
+            <div>
+              <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded" onClick={copyCode}>Copy Code</button>
+              {copied && <span>Code copied!</span>}
+            </div>
+          </div>
+
+        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="products">
+            {(provided) => (
+              <div className='flex flex-row justify-center flex-wrap ' ref={provided.innerRef} {...provided.droppableProps}>
+                {products.map((product, index) => (
+                  <Draggable key={product.id} draggableId={product.id} index={index}>
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className="flex flex-col justify-between  bg-gray-200 px-8 py-8 m-5 rounded-md w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+                      >
+                        <div>
+                          <img src={product.image} alt={product.name} className="w-60 h-60 object-contain" />
+                        </div>
+
+
+
+                        <div>
+                          <div className="mb-2 font-extrabold text-xl md:text-lg lg:text-lg xl:text-xl">Name: {product.name}</div>
+                          <div className="mb-2 font-bold text-xl md:text-base lg:text-lg xl:text-2xl">Price: {product.price.toFixed(0)}Rs</div>
+                          <div className="mb-2 font-semibold text-xl md:text-base lg:text-4xl xl:text-xl">Gender: {product.gender}</div>
+                          <div className="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Color: {product.color}</div>
+                          <div className="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Size: {product.size}</div>
+                        </div>
+                        <div className="flex justify-end">
+                          <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={() => deleteProduct(product.id)}
+                          >
+                            <FontAwesomeIcon icon={faTrash} />
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Homepage.js
@@ -0,0 +1,76 @@
+<<div class="mb-4">
+  <input type="text" placeholder="Enter Image URL" class="border border-gray-400 p-2 rounded m-1" value="">
+    <input type="text" placeholder="Enter Product Name" class="border border-gray-400 p-2 rounded m-1 " value="">
+      <input type="number" placeholder="Enter Price" class="border border-gray-400 p-2 rounded m-1 mx-2" value="">
+        <input type="text" placeholder="Enter Gender" class="border border-gray-400 p-2 rounded m-1" value="">
+          <input type="text" placeholder="Enter Color" class="border border-gray-400 p-2 rounded m-1 mx-2" value="">
+            <input type="text" placeholder="Enter Size" class="border border-gray-400 p-2 rounded m-1" value="">
+              <div class="flex justify-end">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">Add Product</button>
+                <div>
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">Copy Code</button>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-row justify-center flex-wrap " data-rbd-droppable-id="products" data-rbd-droppable-context-id="0">
+              <div data-rbd-draggable-context-id="0" data-rbd-draggable-id="c3147503-b10c-4e72-a868-934ba6870744" tabindex="0" role="button" aria-describedby="rbd-hidden-text-0-hidden-text-0" data-rbd-drag-handle-draggable-id="c3147503-b10c-4e72-a868-934ba6870744" data-rbd-drag-handle-context-id="0" draggable="false" class="flex flex-col justify-between  bg-gray-200 px-8 py-8 m-5 rounded-md w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+                <div>
+                  <img src="https://images.pexels.com/photos/6310924/pexels-photo-6310924.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=600" alt="jeshvi fashion Men Solid Casual Dark Blue, Green Shirt" class="w-60 h-60 object-contain">
+                  </div>
+                  <div>
+                    <div class="mb-2 font-extrabold text-xl md:text-lg lg:text-lg xl:text-xl">Name: jeshvi fashion Men Solid Casual Dark Blue, Green Shirt</div>
+                    <div class="mb-2 font-bold text-xl md:text-base lg:text-lg xl:text-2xl">Price: 670Rs</div>
+                    <div class="mb-2 font-semibold text-xl md:text-base lg:text-4xl xl:text-xl">Gender: Men</div>
+                    <div class="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Color: Dark Blue and Green</div>
+                    <div class="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Size: XL</div>
+                  </div>
+                  <div class="flex justify-end">
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path fill="currentColor" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
+                        </path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div data-rbd-draggable-context-id="0" data-rbd-draggable-id="6966646d-d44f-45c5-9f3e-84e629f64dea" tabindex="0" role="button" aria-describedby="rbd-hidden-text-0-hidden-text-0" data-rbd-drag-handle-draggable-id="6966646d-d44f-45c5-9f3e-84e629f64dea" data-rbd-drag-handle-context-id="0" draggable="false" class="flex flex-col justify-between  bg-gray-200 px-8 py-8 m-5 rounded-md w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+                  <div>
+                    <img src="https://images.pexels.com/photos/9286143/pexels-photo-9286143.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=2" alt="MISS CLOTHING Women Kurta Dupatta Set" class="w-60 h-60 object-contain">
+                    </div>
+                    <div>
+                      <div class="mb-2 font-extrabold text-xl md:text-lg lg:text-lg xl:text-xl">Name: MISS CLOTHING Women Kurta Dupatta Set</div>
+                      <div class="mb-2 font-bold text-xl md:text-base lg:text-lg xl:text-2xl">Price: 690Rs</div>
+                      <div class="mb-2 font-semibold text-xl md:text-base lg:text-4xl xl:text-xl">Gender: Women</div>
+                      <div class="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Color: White and Pink</div>
+                      <div class="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Size: L</div>
+                    </div>
+                    <div class="flex justify-end">
+                      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                          <path fill="currentColor" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
+                          </path>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <div data-rbd-draggable-context-id="0" data-rbd-draggable-id="2d57513d-0bb3-4997-9988-3c8b29ebb562" tabindex="0" role="button" aria-describedby="rbd-hidden-text-0-hidden-text-0" data-rbd-drag-handle-draggable-id="2d57513d-0bb3-4997-9988-3c8b29ebb562" data-rbd-drag-handle-context-id="0" draggable="false" class="flex flex-col justify-between  bg-gray-200 px-8 py-8 m-5 rounded-md w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+                    <div>
+                      <img src="https://via.placeholder.com/150" alt="" class="w-60 h-60 object-contain">
+                      </div>
+                      <div>
+                        <div class="mb-2 font-extrabold text-xl md:text-lg lg:text-lg xl:text-xl">Name: </div>
+                        <div class="mb-2 font-bold text-xl md:text-base lg:text-lg xl:text-2xl">Price: NaNRs</div>
+                        <div class="mb-2 font-semibold text-xl md:text-base lg:text-4xl xl:text-xl">Gender: </div>
+                        <div class="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Color: </div>
+                        <div class="mb-2 font-semibold text-xl md:text-base lg:text-lg xl:text-xl">Size: </div>
+                      </div>
+                      <div class="flex justify-end">
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path fill="currentColor" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
+                            </path>
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>>
